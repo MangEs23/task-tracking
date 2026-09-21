f39149204ea581156b84793a9cf101aaba82a1cc
Alfitka <alfitka_k@ecampus.ut.ac.id>
2026-09-21 16:27:33 +0700

    fix(allow-origins): add localhost:8080

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ app = FastAPI(title="Project Management API")
 # Middleware CORS (dari teman Anda)
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="http://localhost:5173",
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:8080",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
